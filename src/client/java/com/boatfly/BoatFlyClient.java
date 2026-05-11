c8ac2e6de2691232e74dea7af091f5487358075a
Eric Gilerson <itssgilly@gmail.com>
2026-05-11 18:27:54 -0400

    Refactor client, bump mod version & CI
    Update CI to use Java 25 and upload-artifact v4; bump mod_version to 6.1.0. Refactor BoatFlyClient: switch to client command API, remove unused fields, add early null checks, simplify input handling and vehicle movement logic, and clean up the multiplier math for speed calculation. Update fabric.mod.json to use the boatfly icon and set environment to "client".

--- src/client/java/com/boatfly/BoatFlyClient.java
+++ src/client/java/com/boatfly/BoatFlyClient.java
@@ -5,10 +5,10 @@ import com.mojang.brigadier.arguments.FloatArgumentType;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommands;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
-import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.Entity;
@@ -22,12 +22,7 @@ public class BoatFlyClient implements ClientModInitializer {
 	public static KeyMapping BoatSpeedDec;
 	public static double BoatSpeed = 1;
 	public static double boatVelocity = 8;
-	private static double lastPlayerX;
-	private static double lastPlayerZ;
-	private static long lastCheckTime;
-	private static double playerSpeed;
 	private static Vec3 boat;
-	private static final Minecraft client = Minecraft.getInstance();
 	private static final KeyMapping.Category BOATFLY_CATEGORY =
 			KeyMapping.Category.register(Identifier.fromNamespaceAndPath("boatfly", "main"));
 
@@ -45,9 +40,9 @@ public class BoatFlyClient implements ClientModInitializer {
 				onRenderTick(client);
 			}
 		});
-		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-			dispatcher.register(Commands.literal("boatspeed")
-					.then(Commands.argument("value", FloatArgumentType.floatArg())
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(ClientCommands.literal("boatspeed")
+					.then(ClientCommands.argument("value", FloatArgumentType.floatArg())
 							.executes(context -> {
 								final double value = Math.round((FloatArgumentType.getFloat(context, "value")) * 1000.0) / 1000.0;
 								changeSpeed(Minecraft.getInstance(), (float) value, false);
@@ -57,74 +52,56 @@ public class BoatFlyClient implements ClientModInitializer {
 	}
 
 	private void onRenderTick(Minecraft client) {
-		if (client.player != null) {
-			if (BoatFlight.consumeClick()) {
-				BoatFlyOn = !BoatFlyOn;
-				System.out.println(BoatFlyOn);
-				BoatSpeed = 1;
-				if (BoatFlyOn) {
-					client.player.sendOverlayMessage(Component.literal("Boat Fly is now On and Boat Speed is Set to 8 blocks/s"));
-					changeSpeed(client, 8, true);
-				} else {
-					client.player.sendOverlayMessage(Component.literal("Boat Fly is now Off"));
-				}
-			}
-			if (BoatSpeedDec.consumeClick()) {
-				if(boatVelocity != 0) {
-					changeSpeed(client, boatVelocity + -1, false);
-				}
+		if (client.player == null) return;
 
+		if (BoatFlight.consumeClick()) {
+			BoatFlyOn = !BoatFlyOn;
+			BoatSpeed = 1;
+			if (BoatFlyOn) {
+				client.player.sendOverlayMessage(Component.literal("Boat Fly is now On and Boat Speed is Set to 8 blocks/s"));
+				changeSpeed(client, 8, true);
+			} else {
+				client.player.sendOverlayMessage(Component.literal("Boat Fly is now Off"));
 			}
-			if (BoatSpeedInc.consumeClick()) {
-				changeSpeed(client, boatVelocity + 1, false);
+		}
+		if (BoatSpeedDec.consumeClick()) {
+			if (boatVelocity > 0) {
+				changeSpeed(client, Math.max(0, boatVelocity - 1), false);
 			}
-			if (client.options.keyJump.isDown()) {
-				if (BoatFlyClient.BoatFlyOn) {
-					if (!client.player.isPassenger())
-						return;
-					Entity vehicle = client.player.getVehicle();
-					assert vehicle != null;
-					Vec3 velocity = vehicle.getDeltaMovement();
-					double motionY = client.options.keyJump.isDown() ? 0.3 : 0;
-					vehicle.setDeltaMovement(new Vec3(velocity.x, motionY, velocity.z));
-				}
-
-			}
-			if (client.options.keyUp.isDown()) {
-				if (BoatFlyClient.BoatSpeed != 1) {
-					if (!client.player.isPassenger())
-						return;
-					Entity vehicle = client.player.getVehicle();
-					assert vehicle != null;
-					Vec3 velocity = vehicle.getDeltaMovement();
-					boat = new Vec3(velocity.x * BoatSpeed, velocity.y, velocity.z * BoatSpeed);
-					vehicle.setDeltaMovement(boat);
-					assert BoatFlyClient.client.player != null;
-				}
-			}
-
+		}
+		if (BoatSpeedInc.consumeClick()) {
+			changeSpeed(client, boatVelocity + 1, false);
 		}
 
+		Entity vehicle = client.player.getVehicle();
+		if (vehicle == null) return;
 
+		if (client.options.keyJump.isDown() && BoatFlyOn) {
+			Vec3 velocity = vehicle.getDeltaMovement();
+			vehicle.setDeltaMovement(new Vec3(velocity.x, 0.3, velocity.z));
+		}
+		if (client.options.keyUp.isDown() && BoatSpeed != 1) {
+			Vec3 velocity = vehicle.getDeltaMovement();
+			boat = new Vec3(velocity.x * BoatSpeed, velocity.y, velocity.z * BoatSpeed);
+			vehicle.setDeltaMovement(boat);
+		}
 	}
 	public void changeSpeed(Minecraft client, double speed, boolean fly){
-
 		BoatSpeed = multiplier(speed);
 		double scale = Math.pow(10, 5);
 		boatVelocity = Math.round(speed * scale) / scale;
-		double BoatSpeedRound = Math.round(BoatSpeed * scale) / scale;
-		System.out.println(BoatSpeedRound);
 		String StringBoatSpeed = Double.toString(speed);
-		assert client.player != null;
-		if(!fly) {
+		if (!fly && client.player != null) {
 			client.player.sendOverlayMessage(Component.literal("Your Boat Speed is now " + StringBoatSpeed + " blocks/s"));
 		}
 	}
 
 	private double multiplier(double velocity){
 		if (velocity <= 0) return 0;
-		double finalMultiplier;
-		finalMultiplier = Math.pow((-5.33893*Math.pow(Math.log(velocity-8+11.9072),-3.31832)+1.26253),0.470998);
-		return finalMultiplier;
+		double logInput = velocity - 8 + 11.9072;
+		if (logInput <= 0) return 1.0;
+		double base = -5.33893 * Math.pow(Math.log(logInput), -3.31832) + 1.26253;
+		if (base <= 0) return 0;
+		return Math.pow(base, 0.470998);
 	}
 }
